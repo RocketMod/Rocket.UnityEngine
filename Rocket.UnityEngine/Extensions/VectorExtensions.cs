520f0f98a83e32ca7f00b822a2a4b5794f610520
Trojaner_ <trojaner25@gmail.com>
2018-05-15 04:15:41 +0300

    Add VectorExtensions

---
+++ Rocket.UnityEngine/Extensions/VectorExtensions.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Rocket.UnityEngine.Extensions
+{
+    public static class VectorExtensions
+    {
+        public static API.Math.Vector3 ToRocketVector(this Vector3 vector)
+        {
+            return new API.Math.Vector3
+            {
+                X = vector.x,
+                Y = vector.y,
+                Z = vector.z
+            };
+        }
+
+        public static Vector3 ToUnityVector(this API.Math.Vector3 vector)
+        {
+            return new Vector3
+            {
+                x = vector.X,
+                y = vector.Y,
+                z = vector.Z
+            };
+        }
+    }
+}
