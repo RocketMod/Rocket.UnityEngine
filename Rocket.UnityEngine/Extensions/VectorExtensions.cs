9c2e943719aa5d85cb3a834144cc5beb16e01144
Trojaner_ <trojaner25@gmail.com>
2018-05-15 16:03:35 +0300

    Update to latest RocketMod

--- Rocket.UnityEngine/Extensions/VectorExtensions.cs
+++ Rocket.UnityEngine/Extensions/VectorExtensions.cs
@@ -4,9 +4,27 @@ namespace Rocket.UnityEngine.Extensions
 {
     public static class VectorExtensions
     {
-        public static API.Math.Vector3 ToRocketVector(this Vector3 vector)
+        public static System.Numerics.Vector2 ToRocketVector(this Vector2 vector)
         {
-            return new API.Math.Vector3
+            return new System.Numerics.Vector2
+            {
+                X = vector.x,
+                Y = vector.y
+            };
+        }
+
+        public static Vector2 ToUnityVector(this System.Numerics.Vector2 vector)
+        {
+            return new Vector2
+            {
+                x = vector.X,
+                y = vector.Y
+            };
+        }
+
+        public static System.Numerics.Vector3 ToRocketVector(this Vector3 vector)
+        {
+            return new System.Numerics.Vector3
             {
                 X = vector.x,
                 Y = vector.y,
@@ -14,7 +32,7 @@ namespace Rocket.UnityEngine.Extensions
             };
         }
 
-        public static Vector3 ToUnityVector(this API.Math.Vector3 vector)
+        public static Vector3 ToUnityVector(this System.Numerics.Vector3 vector)
         {
             return new Vector3
             {
@@ -23,5 +41,28 @@ namespace Rocket.UnityEngine.Extensions
                 z = vector.Z
             };
         }
+
+        public static System.Numerics.Vector4 ToRocketVector(this Vector4 vector)
+        {
+            return new System.Numerics.Vector4
+            {
+                X = vector.x,
+                Y = vector.y,
+                Z = vector.z,
+                W = vector.w
+            };
+        }
+
+        public static Vector4 ToUnityVector(this System.Numerics.Vector4 vector)
+        {
+            return new Vector4
+            {
+                x = vector.X,
+                y = vector.Y,
+                z = vector.Z,
+                w = vector.W
+            };
+        }
+
     }
 }
