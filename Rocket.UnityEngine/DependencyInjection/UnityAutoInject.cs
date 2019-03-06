996744a8ef1c5180b4a34e018a69c3f03f7bdbb9
Enes Sadık Özbek <es.ozbek@outlook.com>
2019-03-06 20:49:49 +0300

    Add Dependency Injection extensions for Components

---
+++ Rocket.UnityEngine/DependencyInjection/UnityAutoInject.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Rocket.UnityEngine.DependencyInjection
+{
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+    public class UnityAutoInjectAttribute : Attribute
+    {
+
+    }
+}
---
+++ Rocket.UnityEngine/DependencyInjection/UnityInjectionExtensions.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using JetBrains.Annotations;
+using Rocket.API.DependencyInjection;
+using UnityEngine;
+
+namespace Rocket.UnityEngine.DependencyInjection
+{
+    public static class UnityInjectionExtensions
+    {
+        public static T AddComponentWithInjection<T>(this GameObject o, IDependencyContainer container) where T : Component
+        {
+            o.SetActive(false);
+            T addedComponent = o.AddComponent<T>();
+
+            foreach (var property in typeof(T).GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance))
+            {
+                InjectProperty(property, container, addedComponent);
+            }
+
+            foreach (var field in typeof(T).GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance))
+            {
+                InjectField(field, container, addedComponent);
+            }
+
+            o.SetActive(true);
+            return addedComponent;
+        }
+
+        public static void SetupInjections<T>(this Component component, IDependencyContainer container)
+        {
+            GameObject o = component.gameObject;
+            o.SetActive(false);
+            foreach (var property in typeof(T).GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance))
+            {
+                InjectProperty(property, container, component);
+            }
+
+            foreach (var field in typeof(T).GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance))
+            {
+                InjectField(field, container, component);
+            }
+            o.SetActive(true);
+        }
+
+        public static Component AddComponentWithInjection(this GameObject o, Type type, IDependencyContainer container)
+        {
+            o.SetActive(false);
+            Component addedComponent = o.AddComponent(type);
+
+            foreach (var property in type.GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance))
+            {
+                InjectProperty(property, container, addedComponent);
+            }
+
+            foreach (var field in type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance))
+            {
+                InjectField(field, container, addedComponent);
+            }
+
+            o.SetActive(true);
+            return addedComponent;
+        }
+
+        private static void InjectProperty([NotNull] PropertyInfo property, [NotNull] IDependencyContainer container, [NotNull] Component component)
+        {
+            if (property.GetCustomAttributes().Any(d => d is UnityAutoInjectAttribute))
+            {
+                var instance = container.Resolve(property.PropertyType);
+
+                var setMethod = property.GetSetMethod(true);
+                if (setMethod == null)
+                {
+                    throw new Exception($"Could not AutoInject property \"{property.Name}\" in type \"{component.GetType().FullName}\": Setter was not accessible.");
+                }
+                property.GetSetMethod(true).Invoke(component, new[] { instance });
+            }
+        }
+
+        private static void InjectField([NotNull] FieldInfo field, [NotNull] IDependencyContainer container, [NotNull] Component component)
+        {
+
+            if (field.GetCustomAttributes().Any(d => d is UnityAutoInjectAttribute))
+            {
+                var instance = container.Resolve(field.FieldType);
+                field.SetValue(component, instance);
+            }
+        }
+    }
+}
